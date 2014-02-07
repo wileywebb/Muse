af63e9687f77efee1cb768471a0a6f1df7bb1765
Nadav Hollander <nadavh@stanford.edu>
2014-02-06 19:13:49 -0800

    fixed node error

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var tile = require('./routes/tile');
 // Example route
 // var user = require('./routes/user');
 
